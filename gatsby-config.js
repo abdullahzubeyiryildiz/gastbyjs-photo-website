eb1946a2f844ed16c12b921ae354664191909569
Abdullah Zübeyir Yıldız <zubeyir_55@hotmail.com>
2020-05-03 05:42:48 +0300

    Anasayfaya Fotograflar Eklendi.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Photo Website`,
+    description: `Benim ilk fotoğraf sitem`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -11,6 +11,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `photos`,
+        path: `${__dirname}/src/photos`,
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,39 @@
 import React from "react"
+import { graphql } from "gatsby";
+import Img from "gatsby-image"
 
+import './index.css'
 
-const IndexPage = () => (
-    <div>
+const IndexPage = ({data}) => {
+    console.log(data)
+    return <div>
      <h1>Web Site</h1>
-    </div>
-)
 
+    <div className="photos">
+    {data.allFile.edges.map(({node}) => {
+         return  <Img key={node.id} fluid={node.childImageSharp.fluid} />
+     })}
+    </div>
+
+    
+    
+    </div>
+}
+
+export const pageQuery = graphql`
+{
+    allFile(filter: {absolutePath: {regex: "//photos//"}}) {
+      edges {
+        node {
+            id
+            childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+        }
+      }
+    }
+  }
+`
 export default IndexPage
